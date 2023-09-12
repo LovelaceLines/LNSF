b9c9342075d9ceecd628c3336d1298ccbb98a301
George Maia <georgemaiaf@gmail.com>
2023-09-11 21:04:57 -0300

    LNSF - 3.22 - Feat - Adicionar e remover quarto de uma pessoa

--- src/LNSF.Application/Services/PeopleService.cs
+++ src/LNSF.Application/Services/PeopleService.cs
@@ -2,21 +2,25 @@
 using LNSF.Domain.DTOs;
 using LNSF.Domain.Entities;
 using LNSF.Domain.Views;
+using LNSF.Infra.Data.Context;
 
 namespace LNSF.Application;
 
 public class PeopleService
 {
+    private readonly AppDbContext _context;
     private readonly IPeoplesRepository _peopleRepository;
     private readonly IRoomsRepository _roomRepository;
     private readonly PaginationValidator _paginationValidator;
     private readonly PeopleValidator _peopleValidator;
 
-    public PeopleService(IPeoplesRepository peopleRepository,
+    public PeopleService(AppDbContext context,
+        IPeoplesRepository peopleRepository,
         IRoomsRepository roomRepository,
         PaginationValidator paginationValidator,
         PeopleValidator peopleValidator)
     {
+        _context = context;
         _peopleRepository = peopleRepository;
         _roomRepository = roomRepository;
         _paginationValidator = paginationValidator;
@@ -38,89 +42,117 @@ public class PeopleService
     public async Task<ResultDTO<int>> GetQuantity() =>
         await _peopleRepository.GetQuantity();
 
-    public async Task<ResultDTO<People>> Post(People people)
+    public async Task<ResultDTO<People>> CreateNewPerson(People people)
     {
-        if (people.Id != 0) people.Id = 0;
-        
-        if (people.RoomId.HasValue && people.RoomId != 0)
+        var validationResult = _peopleValidator.Validate(people);
+        if (!validationResult.IsValid) return new ResultDTO<People>(validationResult.ToString());
+
+        // CreateNewPerson
+        var transaction = await _context.Database.BeginTransactionAsync();
+
+        var resultPeople = await _peopleRepository.Post(people);
+        if (resultPeople.Error == true)
         {
-            var resultRoom = await _roomRepository.Get(id: people.RoomId.Value);
-
-            if (resultRoom.Error == true) return new ResultDTO<People>("Quarto não encontrado.");
-
-            var room = resultRoom.Data;
-            if (room.Beds - room.Occupation <= 0) return new ResultDTO<People>("Não há vagas.");
+            await transaction.RollbackAsync();
+            return new ResultDTO<People>("Erro ao criar pessoa.");
         }
 
-        var validationResult = _peopleValidator.Validate(people);
+        await transaction.CommitAsync();
 
-        return validationResult.IsValid ?
-            await _peopleRepository.Post(people) :
-            new ResultDTO<People>(validationResult.ToString());
+        return resultPeople;
     }
 
-    public async Task<ResultDTO<People>> Put(People people)
+    public async Task<ResultDTO<People>> EditBasicInformation(People people)
     {
-        if (people.Id == 0) return new ResultDTO<People>("Pessoa não encontrada");
-        if (people.RoomId == 0) return new ResultDTO<People>("Quarto não encontrado");
-
         var validationResult = _peopleValidator.Validate(people);
+        if (!validationResult.IsValid) return new ResultDTO<People>(validationResult.ToString());
 
-        return validationResult.IsValid ?
-            await _peopleRepository.Put(people) :
-            new ResultDTO<People>(validationResult.ToString());
+        var resultPeople = await _peopleRepository.Get(people.Id);
+        if (resultPeople.Error == true) return new ResultDTO<People>("Pessoa não encontrada.");
+
+        people.RoomId = resultPeople.Data.RoomId; //Quarto não pode ser alterado.
+
+        // EditBasicInformation
+        var transaction = await _context.Database.BeginTransactionAsync();
+
+        resultPeople = await _peopleRepository.Put(people);
+        if (resultPeople.Error == true)
+        {
+            await transaction.RollbackAsync();
+            return new ResultDTO<People>("Erro ao editar pessoa.");
+        }
+
+        await transaction.CommitAsync();
+
+        return resultPeople;	
     }
 
     public async Task<ResultDTO<People>> AddPeopleToRoom(int peopleId, int roomId)
     {
         var resultPeople = await _peopleRepository.Get(peopleId);
         if (resultPeople.Error == true) return new ResultDTO<People>("Pessoa não encontrada.");
-        var people = resultPeople.Data;
 
         var resultRoom = await _roomRepository.Get(roomId);
         if (resultRoom.Error == true) return new ResultDTO<People>("Quarto não encontrado.");
+        
+        var people = resultPeople.Data;
         var room = resultRoom.Data;
 
+        if (people.RoomId == roomId) return new ResultDTO<People>("Pessoa já está no quarto.");
+        if (people.RoomId != 0 && people.RoomId != null) return new ResultDTO<People>("Pessoa já está em um quarto.");
         if (room.Beds - room.Occupation <= 0) return new ResultDTO<People>("Não há vagas.");
         if (!room.Available) return new ResultDTO<People>("Quarto indisponível.");
-        if (people.RoomId != 0) return new ResultDTO<People>("Pessoa já está em um quarto.");
-        if (people.RoomId == roomId) return new ResultDTO<People>("Pessoa já está no quarto.");
 
-        //AddPeopleToRoom
+        // AddPeopleToRoom
         people.RoomId = roomId;
         room.Occupation++;
 
-        if ((await _roomRepository.Put(room)).Error == true)
-            return new ResultDTO<People>("Erro ao atualizar quarto.");
-        
+        var transaction = await _context.Database.BeginTransactionAsync();
+
         resultPeople = await _peopleRepository.Put(people);
-        return resultPeople.Error ? 
-            new ResultDTO<People>("Erro ao atualizar pessoa.") :
-            new ResultDTO<People>(resultPeople.Data);
+        resultRoom = await _roomRepository.Put(room);
+
+        if (resultPeople.Error == true || resultRoom.Error == true)
+        {
+            await transaction.RollbackAsync();
+            return new ResultDTO<People>("Erro ao adicionar pessoa ao quarto.");
+        }
+
+        await transaction.CommitAsync();
+
+        return resultPeople;
     }
 
     public async Task<ResultDTO<People>> RemovePeopleFromRoom(int peopleId, int roomId)
     {
         var resultPeople = await _peopleRepository.Get(peopleId);
         if (resultPeople.Error == true) return new ResultDTO<People>("Pessoa não encontrada.");
-        var people = resultPeople.Data;
 
         var resultRoom = await _roomRepository.Get(roomId);
         if (resultRoom.Error == true) return new ResultDTO<People>("Quarto não encontrado.");
+        
+        var people = resultPeople.Data;
         var room = resultRoom.Data;
 
         if (people.RoomId != roomId) return new ResultDTO<People>("Pessoa não está no quarto.");
 
-        //RemovePeopleFromRoom
+        // RemovePeopleFromRoom
         people.RoomId = null;
         room.Occupation--;
 
-        if ((await _roomRepository.Put(room)).Error == true)
-            return new ResultDTO<People>("Erro ao atualizar quarto.");
-        
+        var transaction = await _context.Database.BeginTransactionAsync();
+
         resultPeople = await _peopleRepository.Put(people);
-        return resultPeople.Error ? 
-            new ResultDTO<People>("Erro ao atualizar pessoa.") :
-            new ResultDTO<People>(resultPeople.Data);
+        resultRoom = await _roomRepository.Put(room);
+
+        if (resultPeople.Error == true || resultRoom.Error == true)
+        {
+            await transaction.RollbackAsync();
+            return new ResultDTO<People>("Erro ao remover pessoa do quarto.");
+        }
+        
+        await transaction.CommitAsync();
+
+        return resultPeople;
     }
 }
