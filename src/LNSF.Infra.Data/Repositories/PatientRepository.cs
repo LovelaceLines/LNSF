88091d74adffa7aa477b4d6c0320424890bf7f65
George Maia <georgemaiaf@gmail.com>
2023-10-29 17:00:57 -0300

    LNSF - 5.1 - Fix - Cadastrar Paciente

--- src/LNSF.Infra.Data/Repositories/PatientRepository.cs
+++ src/LNSF.Infra.Data/Repositories/PatientRepository.cs
@@ -3,6 +3,7 @@ using LNSF.Domain.Repositories;
 using LNSF.Infra.Data.Context;
 using LNSF.Domain.Filters;
 using LNSF.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
 namespace LNSF.Infra.Data.Repositories;
 
 public class PatientRepository : BaseRepository<Patient>, IPatientRepository
@@ -17,9 +18,9 @@ public class PatientRepository : BaseRepository<Patient>, IPatientRepository
         var query = _context.Patients.AsNoTracking();
         
         if (filter.Id.HasValue)query = query.Where(x => x.Id == filter.Id);
-        if (filter.PatientId.HasValue)query = query.Where(x => x.PatientId == filter.PatientId);
+        if (filter.PatientId.HasValue)query = query.Where(x => x.PeopleId == filter.PatientId);
         if (filter.HospitalId.HasValue)query = query.Where(x => x.HospitalId == filter.HospitalId);
-        if (filter.SocioEconomicRecord.HasValue)query = query.Where(x => x.SocioEconomicRecord == filter.SocioEconomicRecord);
+        if (filter.SocioEconomicRecord.HasValue)query = query.Where(x => x.SocioeconomicRecord == filter.SocioEconomicRecord);
         if (filter.Term.HasValue)query = query.Where(x => x.Term == filter.Term);
         if (filter.Order == OrderBy.Descending) query = query.OrderByDescending(x => x.Id);
         else query = query.OrderBy(x => x.Id);
