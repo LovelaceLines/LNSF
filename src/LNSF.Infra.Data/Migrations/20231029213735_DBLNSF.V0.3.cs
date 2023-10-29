b83ab0c865cd1fa2db908056cc56db86ca88e553
George Maia <georgemaiaf@gmail.com>
2023-10-29 18:52:41 -0300

    Add DB Migration

---
+++ src/LNSF.Infra.Data/Migrations/20231029213735_DBLNSF.V0.3.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LNSF.src.LNSF.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBLNSFV03 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Escorts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PeopleId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Escorts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Escorts_Peoples_PeopleId",
+                        column: x => x.PeopleId,
+                        principalTable: "Peoples",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PeopleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    HospitalId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SocioeconomicRecord = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Term = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Patients_Hospitals_HospitalId",
+                        column: x => x.HospitalId,
+                        principalTable: "Hospitals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Patients_Peoples_PeopleId",
+                        column: x => x.PeopleId,
+                        principalTable: "Peoples",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Escorts_PeopleId",
+                table: "Escorts",
+                column: "PeopleId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_HospitalId",
+                table: "Patients",
+                column: "HospitalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_PeopleId",
+                table: "Patients",
+                column: "PeopleId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Escorts");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
