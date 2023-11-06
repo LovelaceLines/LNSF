321b1d5983f46ff8eff78558445ccc6029fa28dc
George Maia <georgemaiaf@gmail.com>
2023-11-05 21:26:41 -0300

    LNSF - 7.16 - Feat - Filtro de Ativos, Paciente e Acompanhante para Pessoas

--- src/LNSF.Domain/Filters/PeopleFilter.cs
+++ src/LNSF.Domain/Filters/PeopleFilter.cs
@@ -9,6 +9,9 @@ public class PeopleFilter
     public string? RG { get; set; }
     public string? CPF { get; set; }
     public string? Phone { get; set; }
+    public bool? Patient { get; set; }
+    public bool? Escort { get; set; }
+    public bool? Active { get; set; }
     
     public Pagination Page { get; set; } = new Pagination();
     public OrderBy OrderBy { get; set; } = OrderBy.Ascending;
--- src/LNSF.Infra.Data/Repositories/PeoplesRepository.cs
+++ src/LNSF.Infra.Data/Repositories/PeoplesRepository.cs
@@ -18,13 +18,52 @@ public class PeopleRepository : BaseRepository<People>, IPeopleRepository
     {
         var query = _context.Peoples.AsNoTracking();
 
-        if (filter.Id != null) query = query.Where(x => x.Id == filter.Id);
-        if (!string.IsNullOrEmpty(filter.Name)) query = query.Where(x => x.Name.Contains(filter.Name));
-        if (!string.IsNullOrEmpty(filter.RG)) query = query.Where(x => x.RG.Contains(filter.RG));
-        if (!string.IsNullOrEmpty(filter.CPF)) query = query.Where(x => x.CPF.Contains(filter.CPF));
-        if (!string.IsNullOrEmpty(filter.Phone)) query = query.Where(x => x.Phone.Contains(filter.Phone));
-        if (filter.OrderBy == OrderBy.Descending) query = query.OrderByDescending(x => x.Id);
-        else query = query.OrderBy(x => x.Id);
+        var patients = _context.Patients.AsNoTracking();
+        var hostingsEscorts = _context.HostingsEscorts.AsNoTracking();
+        var escorts = _context.Escorts.AsNoTracking();
+        var hostings = _context.Hostings.AsNoTracking();
+
+        if (filter.Id != null) query = query.Where(p => p.Id == filter.Id);
+        if (!string.IsNullOrEmpty(filter.Name)) query = query.Where(p => p.Name.Contains(filter.Name));
+        if (!string.IsNullOrEmpty(filter.RG)) query = query.Where(p => p.RG.Contains(filter.RG));
+        if (!string.IsNullOrEmpty(filter.CPF)) query = query.Where(p => p.CPF.Contains(filter.CPF));
+        if (!string.IsNullOrEmpty(filter.Phone)) query = query.Where(p => p.Phone.Contains(filter.Phone));
+
+        if (filter.Patient == true) query = query.Where(p => 
+            patients.Any(pt => pt.PeopleId == p.Id));
+        else if (filter.Patient == false) query = query.Where(p =>
+            !patients.Any(pt => pt.PeopleId == p.Id));
+
+        if (filter.Escort == true) query = query.Where(p =>
+            escorts.Any(e => e.PeopleId == p.Id));
+        else if (filter.Escort == false) query = query.Where(p =>
+            !escorts.Any(e => e.PeopleId == p.Id));
+        
+        if (filter.Active == true) query = query.Where(p =>
+            patients.Any(pt => pt.PeopleId == p.Id &&
+                hostings.Any(h =>h.PatientId == pt.Id && 
+                    h.CheckIn <= DateTime.Now && DateTime.Now <= h.CheckOut))
+            ||
+            hostingsEscorts.Any(he => 
+                escorts.Any(e => e.Id == he.EscortId && e.PeopleId == p.Id) 
+                &&
+                hostings.Any(h => h.Id == he.HostingId && 
+                    h.CheckIn <= DateTime.Now && DateTime.Now <= h.CheckOut)
+            ));
+        else if (filter.Active == false) query = query.Where(p =>
+            !patients.Any(pt => pt.PeopleId == p.Id && 
+                hostings.Any(h =>h.PatientId == pt.Id && 
+                    h.CheckIn <= DateTime.Now && DateTime.Now <= h.CheckOut))
+            &&
+            !hostingsEscorts.Any(he =>
+                escorts.Any(e => e.Id == he.EscortId && e.PeopleId == p.Id) 
+                &&
+                hostings.Any(h => h.Id == he.HostingId && 
+                    h.CheckIn <= DateTime.Now && DateTime.Now <= h.CheckOut)
+            ));
+
+        if (filter.OrderBy == OrderBy.Descending) query = query.OrderByDescending(p => p.Name);
+        else query = query.OrderBy(p => p.Name);
 
         var peoples = await query
             .Skip((filter.Page.Page - 1) * filter.Page.PageSize)
