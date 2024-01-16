4c49513bff74c96df8e831b43934da9575d1d737
George Maia <georgemaiaf@gmail.com>
2024-01-15 22:34:39 -0300

    Refactor, Fix

--- src/LNSF.UI/src/Pages/account/registerAccount/TelaDeGerenciamentoAccount.tsx
+++ src/LNSF.UI/src/Pages/account/registerAccount/TelaDeGerenciamentoAccount.tsx
@@ -3,21 +3,21 @@ import { useNavigate, useParams } from "react-router-dom"
 import SupervisedUserCircleRoundedIcon from '@mui/icons-material/SupervisedUserCircleRounded';
 import { ButtonAction } from "../../../Component";
 import { ChangeEvent, useContext, useEffect, useState, } from "react";
-import { AccountContext, iattAccount, iregisterAccount } from "../../../Contexts";
+import { AccountContext, iUser, iattAccount, iregisterAccount } from "../../../Contexts";
 import { Form } from "@unform/web";
 import { IFormErrorsCustom, TextFieldCustom, useCustomForm } from "../../../Component/forms";
 import * as yup from 'yup';
 import { TextSelectCustom } from "../../../Component/forms/TextSelectCustom";
+import { is } from "date-fns/locale";
 
 export const TelaDeGerenciamentoAccount: React.FC = () => {
-
     const { id = 'cadastrar' } = useParams<'id'>();
     const theme = useTheme();
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
     const navigate = useNavigate();
     const [isLoadind, setIsLoading] = useState(false);
     const [valueId, setvalueId] = useState('');
-    const { viewAccount, registerAccount, updateAccount } = useContext(AccountContext);
+    const { getUserById, postUser, postAddUserToRole, putUser, putPassword, deleteRemoveUserFromRole } = useContext(AccountContext);
     const { formRef, save, isSaveAndClose, saveAndClose } = useCustomForm();
     const [isPasswordChange, setIsPasswordChange] = useState(false);
 
@@ -38,13 +38,13 @@ export const TelaDeGerenciamentoAccount: React.FC = () => {
         if (id !== 'cadastrar') {
             setIsLoading(true);
 
-            viewAccount(id)
+            getUserById(id)
                 .then((response) => {
                     if (response instanceof Error) {
                         setIsLoading(false);
                     } else {
-                        formRef.current?.setData(response[0]);
-                        setvalueId(response[0].id)
+                        formRef.current?.setData(response);
+                        setvalueId(response.id)
                         setIsLoading(false);
                     }
                 })
@@ -73,7 +73,7 @@ export const TelaDeGerenciamentoAccount: React.FC = () => {
                         role: Number(dadosValidados.role),
                     }
 
-                    registerAccount(data)
+                    postUser(data)
                         .then((response) => {
                             if (response instanceof Error) {
                                 setIsLoading(false);
@@ -93,10 +93,12 @@ export const TelaDeGerenciamentoAccount: React.FC = () => {
 
                 } else {
 
-                    const data: iattAccount = {
+                    const data: iUser = {
                         id: valueId,
                         userName: dadosValidados.userName,
-                        role: dadosValidados.role,
+                        email: dadosValidados.email,
+                        phone: dadosValidados.phone,
+                        roles: []
                     }
                     const data1: iattAccount = {
                         id: valueId,
@@ -104,7 +106,7 @@ export const TelaDeGerenciamentoAccount: React.FC = () => {
                         oldPassword: dadosValidados.oldPassword,
                     }
 
-                    updateAccount(isPasswordChange ? data1 : data, isPasswordChange)
+                    isPasswordChange ? putUser(data)
                         .then((response) => {
                             if (response instanceof Error) {
                                 setIsLoading(false);
@@ -115,7 +117,20 @@ export const TelaDeGerenciamentoAccount: React.FC = () => {
                         .catch((error) => {
                             setIsLoading(false);
                             console.error('Detalhes do erro:', error);
-                        });
+                        })
+                        :
+                        putUser(data1)
+                            .then((response) => {
+                                if (response instanceof Error) {
+                                    setIsLoading(false);
+                                } else {
+                                    setIsLoading(false);
+                                }
+                            })
+                            .catch((error) => {
+                                setIsLoading(false);
+                                console.error('Detalhes do erro:', error);
+                            });
                 }
             })
             .catch((errors: yup.ValidationError) => {
@@ -130,127 +145,121 @@ export const TelaDeGerenciamentoAccount: React.FC = () => {
             })
     };
 
-    return (
-        <Box
-            display='flex'
-            flexDirection='column'
-            width='100%'
+  return (
+    <Box display='flex' flexDirection='column'>
+      <Box>
+        <Toolbar sx={{ flexGrow: 1, display: 'flex', flexDirection: smDown ? 'column' : 'row', alignItems: smDown ? 'left' : 'flex-end' }}>
+          <Typography
+            variant={smDown ? "h5" : "h4"}
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: 'flex', alignItems: 'flex-end' }}
+          >
+            {!smDown && (<SupervisedUserCircleRoundedIcon color='primary' sx={{ fontSize: '2.7rem', paddingRight: '10px' }} />)}
+            Usuários
+          </Typography>
 
-        >
-            <Box>
-                <Toolbar sx={{ flexGrow: 1, display: 'flex', flexDirection: smDown ? 'column' : 'row', alignItems: smDown ? 'left' : 'flex-end' }}>
-                    <Typography
-                        variant={smDown ? "h5" : "h4"}
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: 'flex', alignItems: 'flex-end' }}
-                    >
-                        {!smDown && (<SupervisedUserCircleRoundedIcon color='primary' sx={{ fontSize: '2.7rem', paddingRight: '10px' }} />)}
-                        Usuários
-                    </Typography>
+          <ButtonAction
+            mostrarBotaoNovo={false}
+            mostrarBotaoApagar={false}
+            mostrarBotaoSalvar={id === 'cadastrar' ? false : true}
+            mostrarBotaoSalvarEFechar={id !== 'cadastrar' ? false : true}
+            aoClicarEmSalvar={id !== 'cadastrar' ? save : undefined}
+            aoClicarEmSalvarEFechar={id === 'cadastrar' ? saveAndClose : undefined}
+            aoClicarEmVoltar={() => window.history.back()}
+          />
+        </Toolbar>
+      </Box>
+      <Divider />
+      <Box style={{ maxHeight: '350px', overflowY: 'auto' }}>
+        <Form ref={formRef} onSubmit={(dados) => handSave(dados)}>
+          <Box margin={1} display='flex' flexDirection='column' >
+            <Grid container direction='column' padding={2} spacing={2}>
+              {isLoadind && (
+                <Grid item>
+                  <LinearProgress variant="indeterminate" />
+                </Grid>
+              )}
+              <Grid item>
+                <Typography variant={smDown ? "h6" : "h5"} >
+                  {(id !== 'cadastrar') ? 'Editar este usuário' : 'Cadastrar um novo usuário'}
+                  <Divider />
+                </Typography>
+              </Grid>
+              <Grid container item direction='row' spacing={2}>
+                <Grid item xs={6}>
+                  <TextFieldCustom
+                    fullWidth
+                    label="Email"
+                    name="userName"
+                    disabled={isLoadind}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextSelectCustom
+                      fullWidth
+                      name="role"
+                      menu={[
+                        {
+                          nome: 'Voluntário',
+                          id: '0'
+                        },
+                        {
+                          nome: 'Administrador',
+                          id: '1'
+                        },
+                        {
+                          nome: 'Assistente Social',
+                          id: '2'
+                        },
+                        {
+                          nome: 'Secretária',
+                          id: '3'
+                        }
+                      ]}
+                    disabled={isLoadind}
+                    label="Função"
+                  />
+                </Grid>
+              </Grid>
+              {id !== 'cadastrar' && (<Grid item xs={6}>
+                <FormControl >
+                  <FormControlLabel
+                    value="start"
+                    control={
+                      <Switch color="primary"
+                        checked={isPasswordChange}
+                        onChange={handleSwitchChange}
+                      />}
+                    label="Alterar Senha?"
+                    labelPlacement="start"
+                  />
+                </FormControl>
+              </Grid>)}
 
-                    < ButtonAction
-                        mostrarBotaoNovo={false}
-                        mostrarBotaoApagar={false}
-                        mostrarBotaoSalvar={id === 'cadastrar' ? false : true}
-                        mostrarBotaoSalvarEFechar={id !== 'cadastrar' ? false : true}
-                        aoClicarEmSalvar={id !== 'cadastrar' ? save : undefined}
-                        aoClicarEmSalvarEFechar={id === 'cadastrar' ? saveAndClose : undefined}
-                        aoClicarEmVoltar={() => { navigate('/inicio/usuarios/gerenciar') }}
-                    />
-                </Toolbar>
+              <Grid container item direction='row' spacing={2}>
+                <Grid item xs={6} >
 
-            </Box>
-            <Divider />
-            <Box style={{ maxHeight: '350px', overflowY: 'auto' }}>
-                <Form ref={formRef} onSubmit={(dados) => handSave(dados)}>
-                    <Box margin={1} display='flex' flexDirection='column' >
-                        <Grid container direction='column' padding={2} spacing={2}>
-                            {isLoadind && (
-                                <Grid item>
-                                    <LinearProgress variant="indeterminate" />
-                                </Grid>
-                            )}
-                            <Grid item>
-                                <Typography variant={smDown ? "h6" : "h5"} >
-                                    {(id !== 'cadastrar') ? 'Editar este usuário' : 'Cadastrar um novo usuário'}
-                                    <Divider />
-                                </Typography>
-                            </Grid>
-                            <Grid container item direction='row' spacing={2}>
-                                <Grid item xs={6}>
-                                    <TextFieldCustom
-                                        fullWidth
-                                        label="Email"
-                                        name="userName"
-                                        disabled={isLoadind}
-                                    />
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <TextSelectCustom
-                                        fullWidth
-                                        name="role"
-                                        menu={[
-                                            {
-                                                nome: 'Voluntário',
-                                                id: '0'
-                                            },
-                                            {
-                                                nome: 'Administrador',
-                                                id: '1'
-                                            },
-                                            {
-                                                nome: 'Assistente Social',
-                                                id: '2'
-                                            },
-                                            {
-                                                nome: 'Secretária',
-                                                id: '3'
-                                            }
-                                        ]}
-                                        disabled={isLoadind}
-                                        label="Função"
-                                    />
-                                </Grid>
-                            </Grid>
-                            {id !== 'cadastrar' && (<Grid item xs={6}>
-                                <FormControl >
-                                    <FormControlLabel
-                                        value="start"
-                                        control={
-                                            <Switch color="primary"
-                                                checked={isPasswordChange}
-                                                onChange={handleSwitchChange}
-                                            />}
-                                        label="Alterar Senha?"
-                                        labelPlacement="start"
-                                    />
-                                </FormControl>
-                            </Grid>)}
-
-                            <Grid container item direction='row' spacing={2}>
-                                <Grid item xs={6} >
-
-                                    <TextFieldCustom
-                                        fullWidth
-                                        label="Senha Nova"
-                                        name="password"
-                                        disabled={isLoadind || !isPasswordChange}
-                                    />
-                                </Grid>
-                                {id !== 'cadastrar' && (<Grid item xs={6}>
-                                    <TextFieldCustom
-                                        fullWidth
-                                        label="Senha Antiga"
-                                        name="oldPassword"
-                                        disabled={isLoadind || !isPasswordChange}
-                                    />
-                                </Grid>)}
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Form>
-            </Box>
-        </Box>
-    )
+                  <TextFieldCustom
+                    fullWidth
+                    label="Senha Nova"
+                    name="password"
+                    disabled={isLoadind || !isPasswordChange}
+                  />
+                </Grid>
+                {id !== 'cadastrar' && (<Grid item xs={6}>
+                  <TextFieldCustom
+                    fullWidth
+                    label="Senha Antiga"
+                    name="oldPassword"
+                    disabled={isLoadind || !isPasswordChange}
+                  />
+                </Grid>)}
+              </Grid>
+            </Grid>
+          </Box>
+        </Form>
+      </Box>
+    </Box>
+  )
 }
