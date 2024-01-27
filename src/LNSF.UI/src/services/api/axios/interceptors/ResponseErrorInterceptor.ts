feb14707131e3e876a120467c677268f54d71870
George Maia <georgemaiaf@gmail.com>
2024-01-27 18:16:05 -0300

    Fix - Refresh Token

--- src/LNSF.UI/src/services/api/axios/interceptors/ResponseErrorInterceptor.ts
+++ src/LNSF.UI/src/services/api/axios/interceptors/ResponseErrorInterceptor.ts
@@ -3,64 +3,53 @@ import { AxiosError, AxiosRequestConfig } from "axios";
 import { iAppException, iDotNetException } from "../../types";
 import { Api } from "../index";
 import { LocalStorage } from "../../../../Global";
-import { refreshToken } from "../../../AuthService";
-
-// 401 unauthorized 403 forbidden 404 not found 500 internal server error
+import { iToken } from "../../../../Contexts";
 
 export const responseErrorInterceptor = async (error: AxiosError) => { 
   console.debug("ResponseErrorInterceptor: ", error);
 
-  // Verifica se o erro é do tipo iAppException
-  if (error.response!.data instanceof Object && 'id' in error.response!.data && 'statusCode' in error.response!.data && 'message' in error.response!.data ) {
+  if (!!error.response && error.response.status == 401) {
+    LocalStorage.setAccessToken(LocalStorage.getRefreshToken() || '');
+    LocalStorage.setTryToRefreshToken();
+
+    Api.get<iToken>('/Auth/refresh-token')
+    .then(res => {
+      const token = res.data;
+      LocalStorage.setAccessToken(token.accessToken);
+      LocalStorage.setRefreshToken(token.refreshToken);
+    })
+    .then(() => {
+      const originalRequestConfig = error.config;
+      originalRequestConfig!.headers['Authorization'] = `Bearer ${LocalStorage.getAccessToken()}`;
+      Api(originalRequestConfig as AxiosRequestConfig);
+      LocalStorage.clearTryToRefreshToken();
+    })
+    .catch(() => {
+      LocalStorage.setTryToRefreshToken();
+      LocalStorage.clearUser();
+      LocalStorage.clearTokens();
+      toast.error('Acesso não autorizado.');
+    })
+    .finally(() => { return });
+  } else if (!!error.response && error.response.status == 403) {
+    toast.error('Acesso negado.');
+    return;
+  } else if (!!error.response && error.response.status == 404) {
+    toast.error('Recurso não encontrado.');
+    return;
+  } else if (!!error.response && error.response.status == 405) {
+    toast.error('Método não permitido.');
+    return;
+  } else if (error.response!.data instanceof Object && 'id' in error.response!.data && 'statusCode' in error.response!.data && 'message' in error.response!.data ) {
     const appException = error.response!.data as iAppException;
     toast.error(appException.message);
     return;
-  }
-
-  // Verifica se o erro é do tipo iDotNetException
-  if (error.response!.data instanceof Object && 'type' in error.response!.data && 'title' in error.response!.data && 'status' in error.response!.data && 'errors' in error.response!.data && 'traceId' in error.response!.data ) {
+  } else if (error.response!.data instanceof Object && 'type' in error.response!.data && 'title' in error.response!.data && 'status' in error.response!.data && 'errors' in error.response!.data && 'traceId' in error.response!.data ) {
     const dotNetException = error.response!.data as iDotNetException;
     toast.error(dotNetException.title);
     return;
-  }
-
-  if (!!error.response && error.response.status == 401) {
-    if (LocalStorage.getTryToRefreshToken()) {
-      LocalStorage.clearTryToRefreshToken();
-      LocalStorage.clearTokens();
-      LocalStorage.clearUser();
-      toast.error('Acesso não autorizado.');
-      return;
-    }
-
-    return refreshToken()
-    .then(() => {
-      const originalRequestConfig = error.config;
-      Api(originalRequestConfig as AxiosRequestConfig);
-      LocalStorage.clearTryToRefreshToken();
-    });
-  }
-
-  if (!!error.response && error.response.status == 403) {
-    toast.error('Acesso negado.');
+  } else {
+    toast.error('Erro inesperado ao receber resposta.');
     return;
   }
-
-  if (!!error.response && error.response.status == 404) {
-    toast.error('Recurso não encontrado.');
-    return;
-  }
-
-  if (!!error.response && error.response.status == 405) {
-    // TODO - Exibir apenas em ambiente de desenvolvimento
-    toast.error('Método não permitido.');
-    return;
-  }
-
-  // TODO - Salvar Log de erro
-
-  // TODO - Erro de Access Token Expirado
-
-  toast.error('Erro inesperado ao receber resposta.');
-  return;
 }
